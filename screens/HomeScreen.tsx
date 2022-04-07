c8321d71b67b0fa50e72807b87d2cfab1325110d
Rakesh Baruah <rbaruah@gmail.com>
2022-04-07 11:06:24 -0500

    rendering new screens with nav stack

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { Button, Text, View } from "react-native";
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen({ navigation }: any) {
+  useEffect(() => {
+    console.log("rendering home screen");
+  }, []);
   return (
     <View>
       <Text>I am the home screen</Text>
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import { Button, Text, View } from "react-native";
 
-export default function PlannerScreen({ navigation }) {
+export default function PlannerScreen({ navigation }: any) {
+  useEffect(() => {
+    console.log("rendering planner screen");
+  }, []);
+
   return (
     <View>
       <Text>I am planner screen</Text>
