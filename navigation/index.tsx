faf706d4224e74b3fd870142a753d5b775215c25
Rakesh Baruah <rbaruah@gmail.com>
2022-04-14 06:57:45 -0500

    add icons

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { FontAwesome, Entypo } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import PlannerScreen from "../screens/PlannerScreen";
@@ -34,12 +35,20 @@ function BottomTabNavigator() {
       <BottomTab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ unmountOnBlur: true }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
       />
       <BottomTab.Screen
         name="Planner"
         component={PlannerScreen}
-        options={{ unmountOnBlur: true }}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="add-to-list" size={size} color={color} />
+          ),
+        }}
       />
     </BottomTab.Navigator>
   );
