d790cf965104dc53698f3a9bb92bb37fbff1dc6e
Rakesh Baruah <rbaruah@gmail.com>
2022-04-17 06:36:02 -0500

    create useWorkouts hook

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -12,19 +12,10 @@ import { Workout } from "../types/data";
 import WorkoutItem from "../components/WorkoutItem";
 import { MontserratText } from "../components/styled/MonterratText";
 import { getWorkouts } from "../storage/workout";
+import { useWorkouts } from "../hooks/useWorkouts";
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
-  const [workouts, setWorkouts] = useState<Workout[]>([]);
-
-  useEffect(() => {
-    async function getData() {
-      const _workouts = await getWorkouts();
-      setWorkouts(_workouts);
-    }
-
-    getData();
-  }, []);
-
+  const workouts = useWorkouts();
   return (
     <View style={styles.container}>
       <MontserratText style={{ fontSize: 30 }}>New Workouts</MontserratText>
